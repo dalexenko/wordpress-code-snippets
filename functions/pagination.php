b9c92d02db32a7f5cc9414f30610e5a71659d3bf
dalexenko <dalexenko@gmail.com>
2020-03-21 20:19:51 +0200

    Create pagination.php
    Пример рабочей универсальной функции пагинации. Вставляется в файл functions.php.

---
+++ functions/pagination.php
@@ -0,0 +1,29 @@
+function the_pagination() {
+        global $wp_query;
+        $big = 999999999;
+        $links = paginate_links(array(
+            'base' => str_replace($big,'%#%',esc_url(get_pagenum_link($big))),
+            'format' => '?paged=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'type' => 'array',
+            'prev_text'    => '«',
+            'next_text'    => '»',
+            'total' => $wp_query->max_num_pages,
+            'show_all'     => false,
+            'end_size'     => false,
+            'mid_size'     => 1,
+            'add_args'     => false,
+            'add_fragment' => '',
+            'before_page_number' => '',
+            'after_page_number' => ''
+        ));
+
+		if( is_array( $links ) ) {
+            echo '<div class="blog-pagination text-center"><ul>';
+            foreach ( $links as $link ) {
+                if ( strpos( $link, 'current' ) !== false ) echo "<li>".$link."</li>";
+                else echo "<li>".$link."</li>";
+            }
+            echo '</ul></div>';
+		}
+    }
